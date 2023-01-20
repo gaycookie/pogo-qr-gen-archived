69e3915badff72b5ba3ab8663a33d5f07f4f739e
GayCookie <contact@gaycookie.dev>
2023-01-20 10:46:24 +0100

    🧹

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === 'production' ? '/PoGO-QR-Gen/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/pogo-qr-gen/' : '/',
   transpileDependencies: true
 })
